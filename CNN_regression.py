0b261d8d3a2eee5a7e48e862cc62bf289823a712
Philippine Griveaud <pipoune@Philippines-MacBook-Air.local>
2020-10-02 13:52:22 +0200

    Data loader problem

--- CNN_regression.py
+++ CNN_regression.py
@@ -6,21 +6,90 @@ Created on Wed Sep 23 16:31:25 2020
 @author: pipoune
 """
 
-from pycbc.waveform import get_fd_waveform
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+import h5py
+import glob
 
-# We will try different component masses and see which gives us the largest 
-chirp_mass = np.arange(1.1966, 1.1996, .0001)
-masses = np.arange(1.4, 1.7, .001)
+"""
+# =================================================================================
+# ============================   Loading waveforms   ==============================
+# =================================================================================
 
-# Variables to store when we've found the max
-hmax, smax, tmax, mmax, nsnr = None, {}, {}, 0, 0
-snrs = []
+import glob
+from  pycbc.types.timeseries import load_timeseries
 
-for m in masses:
-    #Generate a waveform with a given component mass; assumed equal mass, nonspinning
-    hp, hc = get_fd_waveform(approximant="TaylorF2", 
-                             mass1=m, mass2=m, 
-                             f_lower=20, delta_f=0.1)
-    
-  
+dataset = []
+for file in glob.glob(path+'/waveforms_H1*.hdf'):
+    data = load_timeseries(file)
+    dataset.append(data)
+
+
+
+# for i in range(0,len(dataset), 10):
+#     det_strain = dataset[i]
+#     plt.plot(det_strain.sample_times, det_strain, label=f'{i}')
+# plt.legend()
+# plt.xlabel('Time(s)')
+# plt.ylabel('Strain')
+
+"""
+# %%
+
+path = '/Users/pipoune/Desktop/M2/METEOR_4/Datasets'
+
+
+files_list = glob.glob(path+'/waveforms_H1*.hdf')
+
+
+class HDF5Dataset(Dataset):
+
+     def __init__(self, h5path):
+         
+         self.h5path = h5path
+
+     def __getitem__(self, index):
+        
+         with h5py.File(self.h5path, 'r') as f:
+
+             dataset_name = 'data' + str(index)  # do not understand this line             
+             
+             d = f[dataset_name]
+             
+             data = d[:]
+
+             mass = d.attrs['mass']
+             beta = d.attrs['ra']/(2.0*np.pi)
+             lambd = d.attrs['dec']/(2.0*np.pi)
+
+             return data, mass, beta, lambd
+
+     def __len__(self):
+
+         with h5py.File(self.h5path, 'r') as f:
+             return len(f.keys())
+         
+bob = HDF5Dataset(files_list[0])
+
+
+# dataset = []           
+# for file in files_list:
+#     data = HDF5Dataset(file)
+#     dataset.append(data)
+
+# trainloader = DataLoader(dataset)
+
+
+
+
+
+
+
+
+
+
+
