cbe838c32a42f795caa523873f39740bdb2b5366
Philippine Griveaud <pipoune@Philippines-MacBook-Air.local>
2020-10-12 13:35:59 +0200

    Working NVP

--- RealNVP_waveform.py
+++ RealNVP_waveform.py
@@ -6,9 +6,7 @@ Created on Mon Oct  5 13:53:19 2020
 @author: pipoune
 """
 
-import math
 import numpy as np
-import pandas as pd
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -16,9 +14,33 @@ from torch import distributions
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 import h5py
-from sklearn import datasets
 import matplotlib.pyplot as plt
 plt.close('all')
+
+
+
+class HDF5Dataset(Dataset):
+    
+     def __init__(self, h5path):
+         self.h5path = h5path
+
+     def __getitem__(self, index):
+         with h5py.File(self.h5path, 'r') as f:
+
+             dataset_name = 'data_' + str(index)
+             d = f[dataset_name]
+
+             data = d[:]
+
+             ra = d.attrs['ra'] #/(2.0*np.pi)
+             dec = d.attrs['dec'] #/(2.0*np.pi)
+
+             return data, ra, dec
+
+     def __len__(self):
+         with h5py.File(self.h5path, 'r') as f:
+             return len(f.keys())
+         
 
 
 class RealNVP(nn.Module):
@@ -60,98 +82,111 @@ class RealNVP(nn.Module):
         logp = self.prior.log_prob(z)
         x = self.g(z.view(batchSize,-1), w)
         return x
+    
+    def train(self, inputs, epochs, learning_rate):
+        optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate)
+
+        for epoch in range(epochs): 
+            running_loss = 0
+            
+            for i, (data, ra, dec) in enumerate(inputs):
+                
+                x_ra = ra.type(torch.FloatTensor)
+                x_dec = dec.type(torch.FloatTensor)
+                waveform = data.type(torch.FloatTensor)
+                
+                x = torch.cat([x_ra.view(-1,1), x_dec.view(-1,1)], dim=1)
+                # print(x.shape)
+                # print(waveform.shape)
+                loss = - self.log_prob(x,waveform).mean()
+                
+                optimizer.zero_grad()
+                loss.backward(retain_graph=True)
+                optimizer.step()
+                
+                running_loss += loss.item()
+        
+            if epochs >= 101:
+                if epoch % 100 == 99:
+                    print('[Epoch %d] loss: %.3f' %
+                                  (epoch + 1, running_loss/len(inputs)))
+            else:
+                print('[Epoch %d] loss: %.3f' %
+                                      (epoch + 1, running_loss/len(inputs)))           
+        print('Done Training')
+   
 
 
 #%%
 
-# =================================================================================
-# ============================   Loading waveforms   ==============================
-# =================================================================================
 
 path = '/Users/pipoune/Desktop/M2/METEOR_4/Datasets/'
+train_dataset = HDF5Dataset(path + 'waveforms_ra_dec_2500.hdf')
+inputs = DataLoader(train_dataset, batch_size=100, shuffle=True)
 
-class HDF5Dataset(Dataset):
-     def __init__(self, h5path):
-         self.h5path = h5path
-
-     def __getitem__(self, index):
-         with h5py.File(self.h5path, 'r') as f:
-
-             dataset_name = 'data_' + str(index)
-             d = f[dataset_name]
-
-             data = d[:]
-
-             mass = d.attrs['mass']
-             ra = d.attrs['ra']/(2.0*np.pi)
-             dec = d.attrs['dec']/(2.0*np.pi)
-
-             return data, mass, ra, dec
-
-     def __len__(self):
-         with h5py.File(self.h5path, 'r') as f:
-             return len(f.keys())
-         
-
-train_dataset = HDF5Dataset(path + 'waveforms_dataset_ra_dec_reduced.hdf')
-
-trainloader = DataLoader(train_dataset, batch_size=100, shuffle=True)
-
-#%%
-
-
-# =================================================================================
-# ============================ Training Maping function f =========================
-# =================================================================================
 
 nets = lambda: nn.Sequential(nn.Linear(6146,10), nn.LeakyReLU(), nn.Linear(10,10), nn.LeakyReLU(), nn.Linear(10,2), nn.Tanh())
 nett = lambda: nn.Sequential(nn.Linear(6146,10), nn.LeakyReLU(), nn.Linear(10,10), nn.LeakyReLU(), nn.Linear(10,2))
 masks = torch.from_numpy(np.array([[0, 1], [1, 0]] * 3).astype(np.float32))
 prior = distributions.MultivariateNormal(torch.zeros(2).type(torch.FloatTensor), torch.eye(2).type(torch.FloatTensor)) #torch.eye = ones()
 
+
 flow = RealNVP(nets, nett, masks, prior)
 
-optimizer = torch.optim.Adam(flow.parameters(), lr=1e-4)
-    
-for t in range(100):    
-    for i, (data, mass, ra, dec) in enumerate(trainloader):
-        
-        x_ra = ra.type(torch.FloatTensor)
-        x_dec = dec.type(torch.FloatTensor)
-        waveform = data.type(torch.FloatTensor)
-        
-        x = torch.cat([x_ra.view(-1,1), x_dec.view(-1,1)], dim=1)
-        
-        loss = -flow.log_prob(x,waveform).mean()
-        
-        optimizer.zero_grad()
-        loss.backward(retain_graph=True)
-        optimizer.step()
-    
-        # if t % 10 == 0:
-        print('iter %s:' % t, 'loss = %.3f' % loss)
-        
+flow.train(inputs, 200, 1e-4)
 
 
 
 #%%
 
 # =================================================================================
-# ======================= Testing data with "real" waveform =======================
+# ======================= Getting posterior for "real" waveform ===================
 # =================================================================================
 
 
-batch_size = 100
-w_data =  HDF5Dataset(path + 'waveform_real_GW170817.hdf')
 
-for i in range(int(len(w_data[0][0])/batch_size)):
-    w_real = w_data[0][0][i:i+batch_size]
-    
-    w_real = torch.from_numpy(w_real)
-    w_real = w_real.type(torch.FloatTensor)
-    w_real = w_real.view(-1,1)
-    
-    x = flow.sample(batch_size, w_real).detach().numpy()
+w_data =  HDF5Dataset(path + 'waveform_real_GW170817.hdf')
+w_real = w_data[0][0]
+
+ra = w_data[0][1]
+dec = w_data[0][2]
+
+batch_size = 100
+dtype = torch.FloatTensor
+
+x_wf_repeat = torch.from_numpy(np.tile(w_real, (batch_size, 1)))
+
+# Initialise samples
+samples = flow.sample(batch_size, x_wf_repeat.type(dtype)).detach().numpy()
+
+# Make a lot of samples, you can make a loop here to produce more samples
+for i in range(0, 1000):
+    x = flow.sample(batch_size, x_wf_repeat.type(dtype)).detach().numpy()
+    samples = np.vstack([samples, x])
+
+
+# To plot you can use the corner package
+import corner
+
+truths = np.vstack([ra, dec])
+figure = corner.corner(samples,labels=["Right ascension","Declination"],show_titles=True,truths=truths, color='red', bins=50)
+plt.savefig('posterior.png')
+
+
+
+
+#%%
+
+# x = samples
+# z = np.random.multivariate_normal(np.zeros(2), np.eye(2), 1000)
+
+# plt.scatter(z[:,0], z[:,1], s=2)
+# plt.scatter(x[:, 0], x[:, 1], c='r', s = 2)
+# plt.title(r'$X \sim p(X)$')
+
+
+
+
 
 
 
--- Waveform_generator.py
+++ Waveform_generator.py
@@ -14,9 +14,11 @@ from pycbc.detector import Detector
 import h5py
 
 plt.close('all')
+
 # =================================================================================
 # ============================ Saving function in hdf format  =====================
 # =================================================================================
+
 
 def save(datalist, data_params, path):
         with h5py.File(path, 'a') as f:
@@ -24,16 +26,10 @@ def save(datalist, data_params, path):
                 data = datalist[i]
                 params = data_params[i]
                 ds = f.create_dataset(f'data_{i}', data=data)
-                # ds.attrs['start_time'] = float(data.start_time)
-                # ds.attrs['delta_t'] = float(data.delta_t)
                 ds.attrs['ra'] = float(params['ra'])
                 ds.attrs['dec'] = float(params['dec'])
-                ds.attrs['mass'] = float(params['mass'])
+                # ds.attrs['mass'] = float(params['mass'])
 
-
-# =================================================================================
-# ============================ Creation of waveforms ==============================
-# =================================================================================
 
 path = '/Users/pipoune/Desktop/M2/METEOR_4/Datasets/'
 
@@ -41,75 +37,8 @@ datalist = []
 data_params = [] 
 
 
-def waveform_generation_CNN(masses, ra, dec):
+def waveform_generation_CNN(masses, ra, dec, filename):
     for m in masses:
-        #Generate a waveform with a given component mass; assumed equal mass, nonspinning
-        hp, hc = get_td_waveform(approximant="TaylorF2", 
-                                 mass1=m, mass2=m, 
-                                 f_lower=40.0, delta_t=1.0/1024)
-        
-        # Map to how a detector would see the signal for a specific sky location and time
-        right_ascension = ra # Radians
-        declination = dec
-        polarization = 0
-        hp.start_time = hc.start_time = 0 # GPS seconds
-        params = {'ra':ra, 'dec': dec, 'mass':m}
-
-        
-        for name in ['L1', 'H1', 'V1']:
-            det = Detector(name)
-            det_strain = det.project_wave(hp, hc, 
-                                         right_ascension, declination,
-                                         polarization)
-            det_strain = det_strain.crop(40,9)
-            datalist.append(det_strain)
-            data_params.append(params)
-            plt.plot(det_strain.sample_times, det_strain, label=f'{name}')
-            
-    # save(datalist, data_params, path + 'waveforms_dataset_mass_croped_testset.hdf')
-    
-    
-def real_waveform_generation(ra, dec):
-  m = 1.5
-    #Generate a waveform with a given component mass; assumed equal mass, nonspinning
-  hp, hc = get_td_waveform(approximant="TaylorF2", 
-                             mass1=m, mass2=m, 
-                             f_lower=40.0, delta_t=1.0/(1024))
-    
-    # Map to how a detector would see the signal for a specific sky location and time
-  right_ascension = ra # Radians
-  declination = dec
-  polarization = 0
-
-  hL, hH, hV = [], [], []
-  for name in ['L1', 'H1', 'V1']:
-        det = Detector(name)
-        det_strain = det.project_wave(hp, hc, 
-                                     right_ascension, declination,
-                                     polarization)
-    
-        det_strain = det_strain.time_slice(-12, -10) #need len to be ca 2000
-        # print(len(det_strain))
-        if name == 'L1':
-            hL = (det_strain.numpy())
-        if name == 'H1':
-            hH = (det_strain.numpy())
-        else: 
-            hV = (det_strain.numpy())
-  concat = np.concatenate((hL,hH,hV), axis = 0 )
-  # print(len(concat))
-  datalist.append(concat)
-  # print(np.shape(datalist))
-  params = {'ra':ra, 'dec': dec, 'mass':m}
-  data_params.append(params)
-  # print(len(data_params))
-  save(datalist, data_params, path + 'waveform_real_GW170817.hdf')
-                   
-    
-def waveform_generation_NVP(mass, ra_range, dec_range):
-    m = mass
-    for ra in ra_range:
-        for dec in dec_range:
             #Generate a waveform with a given component mass; assumed equal mass, nonspinning
             hp, hc = get_td_waveform(approximant="TaylorF2", 
                                      mass1=m, mass2=m, 
@@ -135,35 +64,115 @@ def waveform_generation_NVP(mass, ra_range, dec_range):
                     hH = (det_strain.numpy())
                 else: 
                     hV = (det_strain.numpy())
+                    
             concat = np.concatenate((hL,hH,hV), axis = 0 )
-            # print(len(concat))
             datalist.append(concat)
-            # print(np.shape(datalist))
             params = {'ra':ra, 'dec': dec, 'mass':m}
             data_params.append(params)
-    # print(len(data_params))
-    save(datalist, data_params, path + 'waveform_real_GW170817.hdf')
+
+    save(datalist, data_params, path + f'{filename}.hdf')
+    
+    
+def real_waveform_generation(ra, dec):
+      m1 = 1.81
+      m2 = 1.11
+        #Generate a waveform with a given component mass; assumed equal mass, nonspinning
+      hp, hc = get_td_waveform(approximant="TaylorF2", 
+                                 mass1=m1, mass2=m2, 
+                                 f_lower=40.0, delta_t=1.0/(1024))
+        
+        # Map to how a detector would see the signal for a specific sky location and time
+      right_ascension = ra # Radians
+      declination = dec
+      polarization = 0
+    
+      hL, hH, hV = [], [], []
+      for name in ['L1', 'H1', 'V1']:
+            det = Detector(name)
+            det_strain = det.project_wave(hp, hc, 
+                                         right_ascension, declination,
+                                         polarization)
+        
+            det_strain = det_strain.time_slice(-12, -10) #need len to be ca 2000
+            # print(len(det_strain))
+            if name == 'L1':
+                hL = (det_strain.numpy())
+            if name == 'H1':
+                hH = (det_strain.numpy())
+            else: 
+                hV = (det_strain.numpy())
+      concat = np.concatenate((hL,hH,hV), axis = 0 )
+      # print(len(concat))
+      datalist.append(concat)
+      # print(np.shape(datalist))
+      params = {'ra':ra, 'dec': dec}
+      data_params.append(params)
+      # print(len(data_params))
+      save(datalist, data_params, path + 'waveform_real_GW170817.hdf')
+                   
+    
+def waveform_generation_NVP(ra_range, dec_range, filename):
+    m1 = 1.81
+    m2 = 1.11
+    for ra in ra_range:
+        for dec in dec_range:
+            #Generate a waveform with a given component mass; assumed equal mass, nonspinning
+            hp, hc = get_td_waveform(approximant="TaylorF2", 
+                                     mass1=m1, mass2=m2, 
+                                     f_lower=40.0, delta_t=1.0/(1024))
+            
+            # Map to how a detector would see the signal for a specific sky location and time
+            right_ascension = ra # Radians
+            declination = dec
+            polarization = 0
+            # hp.start_time = hc.start_time = 0 # GPS seconds
+            hL, hH, hV = [], [], []
+            for name in ['L1', 'H1', 'V1']:
+                det = Detector(name)
+                det_strain = det.project_wave(hp, hc, 
+                                             right_ascension, declination,
+                                             polarization)
+
+                det_strain = det_strain.time_slice(-12, -10) #need len to be ca 2000
+                # print(len(det_strain))
+                if name == 'L1':
+                    hL = (det_strain.numpy())
+                if name == 'H1':
+                    hH = (det_strain.numpy())
+                else: 
+                    hV = (det_strain.numpy())
+                    
+            concat = np.concatenate((hL,hH,hV), axis = 0 )
+            datalist.append(concat)
+            params = {'ra':ra, 'dec': dec}
+            data_params.append(params)
+
+    save(datalist, data_params, path + f'{filename}.hdf')
             
 
 
 
-N_samples = 30
+
+N_samples = 50
 ra_range = np.random.uniform(0, 2*np.pi, N_samples) 
 dec_range = np.random.uniform(0, 2*np.pi, N_samples)
+filename = 'waveforms_ra_dec_2500'
+waveform_generation_NVP(ra_range, dec_range, filename)
 
-mass = 1.5
-
-# waveform_generation_CNN(masses_test, ra, dec)
-
-# waveform_generation_NVP(mass, ra_range, dec_range)
 
 ra_real = 3.44
-dec_real = 0.41
-
-# real_waveform_generation(ra_real, dec_real)
+dec_real = 2*np.pi -0.44
+real_waveform_generation(ra_real, dec_real)
 
 
 #%%
+# masses = np.random.uniform(1.4,1.7,100)
+# ra = 1.7
+# dec = 1.
+# filename = 'waveforms_dataset_CNN_test'
+# waveform_generation_CNN(masses, ra, dec, filename)
+
+
 
 # =================================================================================
 # ============================ Simple Load and plot  ==============================
